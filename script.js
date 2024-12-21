538d510b717d584c15751bc2e512ecbc06d1a649
Suwenir <Onnywioski@gmail.com>
2024-12-21 17:14:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+// Funkcja zmieniająca slajdy
+function changeSlide() {
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(-${currentIndex * 100}%)`;
+    });
+
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+}
+
+// Zmiana co 3 sekundy
+setInterval(changeSlide, 3000);
